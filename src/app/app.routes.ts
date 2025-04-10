af80c5f15bb4efeea318157da047acac3fb8e8d6
jaroslaw <jaroslawgrabarczyk01@gmail.com>
2025-04-10 13:36:07 +0200

    add load character page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { SheetComponent } from './dark-heresy-ii/sheet/sheet.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { DarkHeresyIICreatorComponent } from './dark-heresy-ii/dark-heresy-ii-creator/dark-heresy-ii-creator.component';
+import { SheetComponent } from 'dark-heresy-ii/sheet/sheet.component';
+import { MainPageComponent } from 'main-page/main-page.component';
+import { DarkHeresyIICreatorComponent } from 'dark-heresy-ii/dark-heresy-ii-creator/dark-heresy-ii-creator.component';
+import { LoadCharacterComponent } from 'load-character/load-character.component';
 
 export const routes: Routes = [
     {path: '', component: MainPageComponent},
     {path: 'sheet', component: SheetComponent},
     {path: 'creator', component: DarkHeresyIICreatorComponent},
+    {path: 'load', component: LoadCharacterComponent},
 ];
---
+++ src/app/load-character/load-character.component.ts
@@ -0,0 +1,53 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { LocalstorageService, StoragePrefixes } from 'services/localstorage.service';
+import { MainNavigationComponent } from '../shared/main-navigation/main-navigation.component';
+import { MatListModule } from '@angular/material/list';
+import { DHII_SheetService } from 'dark-heresy-ii/service/dhii-sheet.service';
+import { DHII_Character } from '@dhii/types/dark-heresy-ii';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '@shared/dialogs/confirm-dialog/confirm-dialog.component';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+const PREFIX: StoragePrefixes = 'dhii';
+
+@Component({
+  selector: 'app-load-character',
+  standalone: true,
+  imports: [MainNavigationComponent, MatCardModule, MatListModule, MatButtonModule, CommonModule],
+  templateUrl: './load-character.component.html',
+  styleUrl: './load-character.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoadCharacterComponent {
+  private dialog: MatDialog = inject(MatDialog);
+  private storageService = inject(LocalstorageService);
+  private sheetService = inject(DHII_SheetService);
+  private router = inject(Router);
+
+  saveNames$ = this.storageService.DHII_CharacterKeys$;
+
+  loadCharacter(item: string): void {
+    const character: DHII_Character = this.storageService.getItem<DHII_Character>(
+      `${PREFIX}+${item}`
+    );
+    this.sheetService.updateCharacter(character);
+    this.router.navigate(['/sheet']);
+  }
+
+  deleteCharacter(item: string): void {
+    this.dialog
+      .open(ConfirmDialogComponent, {
+        data: {
+          text: 'Are you sure you want to delete the save "' + item + '"',
+          title: 'Confirm deletion'
+        }
+      })
+      .afterClosed()
+      .pipe(filter(data => data))
+      .subscribe(() => this.storageService.removeItem(`${PREFIX}+${item}`));
+  }
+}
