9aec807b69cf56e766bb3e537b8babfa08e92d2f
jaroslaw <jaroslawgrabarczyk01@gmail.com>
2025-04-10 13:34:19 +0200

    add confirm dialog and save dialog

---
+++ src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+  templateUrl: './confirm-dialog.component.html',
+  styleUrl: './confirm-dialog.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ConfirmDialogComponent {
+  private dialogRef: MatDialogRef<ConfirmDialogComponent> = inject(
+    MatDialogRef<ConfirmDialogComponent>
+  );
+
+  data = inject(MAT_DIALOG_DATA);
+
+  confirm(): void {
+    this.dialogRef.close(true);
+  }
+}
---
+++ src/app/shared/dialogs/save-dialog/save-dialog.component.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-save-dialog',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule
+  ],
+  templateUrl: './save-dialog.component.html',
+  styleUrl: './save-dialog.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SaveDialogComponent {
+  dialogRef: MatDialogRef<SaveDialogComponent> = inject(MatDialogRef<SaveDialogComponent>)
+  saveName = new FormControl(null, Validators.required);
+
+  save(): void {
+    this.dialogRef.close(this.saveName.value)
+  }
+}
