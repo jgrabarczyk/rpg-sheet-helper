f8358b6236d788f412c21988b3aa7abd320cfcc2
jaroslaw <jaroslawgrabarczyk01@gmail.com>
2025-04-28 17:31:11 +0200

    remove NoopAnimationsModule in save-dialog tests

--- src/app/shared/dialogs/save-dialog/save-dialog.component.spec.ts
+++ src/app/shared/dialogs/save-dialog/save-dialog.component.spec.ts
@@ -1,15 +1,15 @@
-/* eslint-disable @typescript-eslint/typedef */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SaveDialogComponent } from './save-dialog.component';
 import { MatDialogRef } from '@angular/material/dialog';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HarnessLoader } from '@angular/cdk/testing';
 import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { MatInputHarness } from '@angular/material/input/testing';
 
 import { dialogRefMock } from '../../../../tests/mocks/dialog-ref';
 import { MatButtonHarness } from '@angular/material/button/testing';
+import { getButtonHarnessWithSelector } from '../../../../tests/harness-selector-helpers';
 
 describe('SaveDialogComponent', () => {
   let fixture: ComponentFixture<SaveDialogComponent>;
@@ -17,8 +17,8 @@ describe('SaveDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SaveDialogComponent],
-      providers: [{ provide: MatDialogRef, useValue: dialogRefMock }, provideAnimations()]
+      imports: [SaveDialogComponent, NoopAnimationsModule],
+      providers: [{ provide: MatDialogRef, useValue: dialogRefMock }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SaveDialogComponent);
@@ -26,11 +26,11 @@ describe('SaveDialogComponent', () => {
   });
 
   it('should correclty save data', async () => {
-    const inputValue: string = 'custom save'; 
-    const input = await loader.getHarness(MatInputHarness);
-    await input.setValue(inputValue)
-    const button = await loader.getHarness(MatButtonHarness.with({text: 'Save'}))
-    await button.click()
-    expect(dialogRefMock.close).toHaveBeenCalledWith(inputValue)
+    const inputValue: string = 'custom save';
+    const saveNameInput: MatInputHarness = await loader.getHarness(MatInputHarness);
+    await saveNameInput.setValue(inputValue);
+    const saveButton: MatButtonHarness = await getButtonHarnessWithSelector(loader, 'save');
+    await saveButton.click();
+    expect(dialogRefMock.close).toHaveBeenCalledWith(inputValue);
   });
 });
