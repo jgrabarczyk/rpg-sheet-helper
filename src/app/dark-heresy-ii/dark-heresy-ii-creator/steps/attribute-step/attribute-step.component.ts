2c4212037e1d1d7e4f368459302eb5177720d937
jaroslaw <jaroslawgrabarczyk01@gmail.com>
2025-04-29 17:57:08 +0200

    rename isRerollAtributeAvalible to isAtributeRerollAvalible. refactor AttributeStepComponent and AttributesGroupComponent

--- src/app/dark-heresy-ii/dark-heresy-ii-creator/steps/attribute-step/attribute-step.component.ts
+++ src/app/dark-heresy-ii/dark-heresy-ii-creator/steps/attribute-step/attribute-step.component.ts
@@ -2,21 +2,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
+import { Roll } from '@appTypes/roll';
 import { AttributesGroupComponent } from '@dhii/partials/attributes-group/attributes-group.component';
 import { TwoColumnStepComponent } from '@dhii/stepper-partials/two-column-step/two-column-step.component';
 import { DHII_Attributes, DHII_AttributeName } from '@dhii/types/dhii-attribute';
 
-import { Roll } from 'types/roll';
-
 @Component({
   selector: 'app-attribute-step',
   standalone: true,
-  imports: [
-    AttributesGroupComponent,
-    TwoColumnStepComponent,
-    MatButtonModule,
-    MatCardModule,
-],
+  imports: [AttributesGroupComponent, TwoColumnStepComponent, MatButtonModule, MatCardModule],
   templateUrl: './attribute-step.component.html',
   styleUrl: './attribute-step.component.scss'
 })
@@ -24,21 +18,21 @@ export class AttributeStepComponent {
   @Input() attributes!: DHII_Attributes;
   @Input() bonus!: [DHII_AttributeName, DHII_AttributeName];
   @Input() penality!: DHII_AttributeName;
-  isRerollAtributeAvalible: boolean = false;
 
   @Output() reroll = new EventEmitter();
   @Output() generateAll = new EventEmitter();
 
-  valid = false;
+  protected isAtributeRerollAvalible: boolean = false;
+  protected valid = false;
 
-  roll() {
-    this.isRerollAtributeAvalible = true;
+  protected roll(): void {
+    this.isAtributeRerollAvalible = true;
     this.valid = true;
     this.generateAll.emit();
   }
 
-  useReroll($event: Roll) {
-    this.isRerollAtributeAvalible = false;
+  protected useReroll($event: Roll): void {
+    this.isAtributeRerollAvalible = false;
     this.reroll.emit($event);
   }
 }
